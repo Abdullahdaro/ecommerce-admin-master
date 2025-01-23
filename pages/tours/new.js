a57bfb0f05f4f13f1a89ff10235829b4dda8c9ee
AbdullahDaro <abode.morx@gmail.com>
2025-01-23 14:38:31 +0300

    add

---
+++ pages/tours/new.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+import TourForm from "@/components/TourForm";
+
+export default function NewTour() {
+  return (
+    <Layout>
+      <h1>New Tour</h1>
+      <TourForm />
+    </Layout>
+  );
+} 
